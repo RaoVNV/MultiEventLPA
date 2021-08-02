07cb5dc5e0f05594e3ae4bc9710f03cbd931a426
Anton Hesse <ahesse2567@gmail.com>
2021-08-02 13:38:56 -0500

    All the athlete links, beginning of athlete results

--- meet_links.R
+++ meet_links.R
@@ -37,25 +37,42 @@ meetLinks <- getMeetLinks(years)
 meetDivisions <- divisions(length(meetLinks), 40)
 athleteLinksList <- vector(mode = "list", length = length(meetDivisions))
 
-# for(i in 1:length(athleteLinksList)) {
-#     athleteLinksList[[i]] <- getAthleteLinks(meetLinks[meetDivisions[[i]]])
-# }
-
+# rerun now that all the errors *should* be fixed
 for(i in 1:length(athleteLinksList)) {
     athleteLinksList[[i]] <- getAthleteLinks(meetLinks[meetDivisions[[i]]])
     print(i)
 }
 
+# this should be mapped in the future to improve performance
+profvis({
+    als <- map_dfr(small, getAthleteLinks)
+})
+
+
 athleteLinks <- bind_rows(athleteLinksList)
 # write_csv(athleteLinks, "athleteLinks31-40.csv")
 
-athleteDivisions <- divisions(length(athleteLinksList), 40)
-resList <- vector(mode = "list", length = length(sections))
+athleteLinks1_10 <- read_csv("athleteLinks1-10.csv")
+athleteLinks10_30 <- read_csv("athleteLinks10-30.csv")
+athleteLinks31_40 <- read_csv("athleteLinks31-40.csv")
+athleteLinks <- bind_rows(athleteLinks1_10,
+                          athleteLinks10_30,
+                          athleteLinks31_40) %>% 
+    mutate(sex = as.factor(sex))
 
-for(i in 1:length(resList)) {
-    resList[[i]] <- getMultiRes(athleteLinks[sections[[i]],])
+# write_csv(athleteLinks, "athleteLinks.csv")
+
+athleteDivisions <- divisions(nrow(athleteLinks), 50)
+resList <- vector(mode = "list", length = length(athleteDivisions))
+
+# this one is gonna take a while
+# currently made it through 3
+for(i in 3:length(resList)) {
+    resList[[i]] <- getMultiRes(athleteLinks[athleteDivisions[[i]],])
+    print(i)
 }
-res <- getMultiRes(athleteLinks[sections[[1]],])
 
+athleteRes <- bind_rows(resList)
 
+write_csv(athleteRes, "athlete_results.csv")
 
